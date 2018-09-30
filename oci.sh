ed5c2b8d50502a883713c98c91c7fa4bfc158930
xhdix <xhdix@yahoo.com>
2018-09-30 23:54:28 +0300

    Update oci.sh

--- oci.sh
+++ oci.sh
@@ -54,23 +54,23 @@ certtool --generate-privkey --outfile server-key.pem
 certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
 cp server-cert.pem server-key.pem /etc/ocserv
 
-sed -i 's@auth="pam[gid-min=1000]"@auth="plain[/etc/ocserv/ocpasswd]"@' /etc/ocserv/ocserv.conf
-sed -i 's/try-mtu-discovery=false/try-mtu-discovery=true/' /etc/ocserv/ocserv.conf
-sed -i 's/dns=192.168.1.2/dns=1.1.1.1\ndns=8.8.8.8/' /etc/ocserv/ocserv.conf
-sed -i 's/try-mtu-discovery=false/try-mtu-discovery=true/' /etc/ocserv/ocserv.conf
-sed -i 's@server-cert=/etc/ssl/certs/ssl-cert-snakeoil.pem@server-cert=/etc/ocserv/server-cert.pem@' /etc/ocserv/ocserv.conf
-sed -i 's@server-key=/etc/ssl/private/ssl-cert-snakeoil.key@server-key=/etc/ocserv/server-key.pem@' /etc/ocserv/ocserv.conf
-sed -i s/ipv4-network = 192.168.1.0/ipv4-network = 192.168.129.0/ /etc/ocserv/ocserv.conf
-sed -i s@route = 10.10.10.0/255.255.255.0@#route = 10.10.10.0/255.255.255.0@ /etc/ocserv/ocserv.conf
-sed -i s@route = 192.168.0.0/255.255.0.0@route = 192.168.0.0/255.255.0.0@ /etc/ocserv/ocserv.conf
-sed -i s@route = fef4:db8:1000:1001::/64@#route = fef4:db8:1000:1001::/64@ /etc/ocserv/ocserv.conf
-sed -i s@no-route = 192.168.5.0/255.255.255.0@#no-route = 192.168.5.0/255.255.255.0@ /etc/ocserv/ocserv.conf
+sed -i 's/auth = "pam\[gid-min=1000]"/auth = "plain\[\/etc\/ocserv\/ocpasswd]"/g' /etc/ocserv/ocserv.conf
+sed -i 's/try-mtu-discovery = false/try-mtu-discovery = true/' /etc/ocserv/ocserv.conf
+sed -i 's/dns = 192.168.1.2/dns = 1.1.1.1\ndns = 8.8.8.8/' /etc/ocserv/ocserv.conf
+sed -i 's/#tunnel-all-dns = true/tunnel-all-dns = true/' /etc/ocserv/ocserv.conf
+sed -i 's/server-cert=\/etc\/ssl\/certs\/ssl-cert-snakeoil.pem/server-cert=\/etc\/ocserv\/server-cert.pem/' /etc/ocserv/ocserv.conf
+sed -i 's/server-key=\/etc\/ssl\/private\/ssl-cert-snakeoil.key/server-key=\/etc\/ocserv\/server-key.pem/' /etc/ocserv/ocserv.conf
+sed -i 's/ipv4-network = 192.168.1.0/ipv4-network = 192.168.129.0/' /etc/ocserv/ocserv.conf
+sed -i 's/route = 10.10.10.0\/255.255.255.0/#route = 10.10.10.0\/255.255.255.0/' /etc/ocserv/ocserv.conf
+sed -i 's/route = 192.168.0.0\/255.255.0.0/#route = 192.168.0.0\/255.255.0.0/' /etc/ocserv/ocserv.conf
+sed -i 's/route = fef4:db8:1000:1001::\/64/#route = fef4:db8:1000:1001::\/64/' /etc/ocserv/ocserv.conf
+sed -i 's/no-route = 192.168.5.0\/255.255.255.0/#no-route = 192.168.5.0\/255.255.255.0/' /etc/ocserv/ocserv.conf
 
 iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 iptables -I INPUT -p udp --dport 443 -j ACCEPT
 
-sed -i s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/ /etc/sysctl.conf
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 
 sysctl -p /etc/sysctl.conf
 
@@ -84,8 +84,8 @@ systemctl mask ocserv.socket
 
 cp /lib/systemd/system/ocserv.service /etc/systemd/system/ocserv.service
 
-sed -i s/Requires=ocserv.socket/#Requires=ocserv.socket/ /etc/systemd/system/ocserv.service
-sed -i s/Also=ocserv.socket/#Also=ocserv.socket/ /etc/systemd/system/ocserv.service
+sed -i 's/Requires=ocserv.socket/#Requires=ocserv.socket/' /etc/systemd/system/ocserv.service
+sed -i 's/Also=ocserv.socket/#Also=ocserv.socket/' /etc/systemd/system/ocserv.service
 
 systemctl daemon-reload
 systemctl stop ocserv.socket
