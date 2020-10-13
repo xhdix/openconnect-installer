99c49f1b8e179e0d4a9c1462ed49d061cc026ff8
xhdix <xhdix@yahoo.com>
2020-10-14 00:35:08 +0300

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -6,8 +6,10 @@ if [[ $LIST == "" ]] ; then
   exit
 else
   while read -r -a line; do
-          echo "For user ${line[0]} password is update with ${line[1]}"
+    if [[ "${line[0]}" != "" ]] ; then
+    echo "For user ${line[0]} password updated with ${line[1]}"
     echo "${line[1]}" | ocpasswd -c /etc/ocserv/ocpasswd "${line[0]}"
+    fi
   done < $LIST
   exit
 fi
