ba48903b9e80585cf3fd8334ec79d17d5573fd5e
xhdix <xhdix@yahoo.com>
2020-10-26 00:47:10 +0300

    fix sed issue

--- copyoc.sh
+++ copyoc.sh
@@ -59,46 +59,46 @@ if [[ ! $UPDATE ]] ; then
     POSTFIX="$PORT"
     cp -f /etc/ocserv/ocserv.conf /etc/ocserv/ocserv$POSTFIX.conf &
     wait
-    sed -i 's/tcp-port = 443/tcp-port = 2083/' /etc/ocserv/ocserv$POSTFIX.conf
-    sed -i 's/udp-port = 443/#udp-port = 443/' /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/tcp-port = 443/tcp-port = 2083/" /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/udp-port = 443/#udp-port = 443/" /etc/ocserv/ocserv$POSTFIX.conf
     iptables -I INPUT -p tcp --dport 2083 -j ACCEPT &
   elif [[ $PORT != "" ]] && [[ $TPORT == "" ]] && [[ $UPORT == "" ]] ; then
     POSTFIX="$PORT"
     cp -f /etc/ocserv/ocserv.conf /etc/ocserv/ocserv$POSTFIX.conf &
     wait
-    sed -i 's/tcp-port = 443/tcp-port = $PORT/' /etc/ocserv/ocserv$POSTFIX.conf
-    sed -i 's/udp-port = 443/udp-port = $PORT/' /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/tcp-port = 443/tcp-port = $PORT/" /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/udp-port = 443/udp-port = $PORT/" /etc/ocserv/ocserv$POSTFIX.conf
     iptables -I INPUT -p tcp --dport $PORT -j ACCEPT &
     iptables -I INPUT -p udp --dport $PORT -j ACCEPT &
   elif [[ $PORT == "" ]] && [[ $TPORT != "" ]] && [[ $UPORT != "" ]] ; then
     POSTFIX="$TPORT"
     cp -f /etc/ocserv/ocserv.conf /etc/ocserv/ocserv$POSTFIX.conf &
     wait
-    sed -i 's/tcp-port = 443/tcp-port = $TPORT/' /etc/ocserv/ocserv$POSTFIX.conf
-    sed -i 's/udp-port = 443/udp-port = $UPORT/' /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/tcp-port = 443/tcp-port = $TPORT/" /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/udp-port = 443/udp-port = $UPORT/" /etc/ocserv/ocserv$POSTFIX.conf
     iptables -I INPUT -p tcp --dport $TPORT -j ACCEPT &
     iptables -I INPUT -p udp --dport $UPORT -j ACCEPT &
   elif [[ $PORT == "" ]] && [[ $TPORT != "" ]] && [[ $UPORT == "" ]] ; then
     POSTFIX="$TPORT"
     cp -f /etc/ocserv/ocserv.conf /etc/ocserv/ocserv$POSTFIX.conf &
     wait
-    sed -i 's/tcp-port = 443/tcp-port = $TPORT/' /etc/ocserv/ocserv$POSTFIX.conf
-    sed -i 's/udp-port = 443/#udp-port = 443/' /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/tcp-port = 443/tcp-port = $TPORT/" /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/udp-port = 443/#udp-port = 443/" /etc/ocserv/ocserv$POSTFIX.conf
     iptables -I INPUT -p tcp --dport $TPORT -j ACCEPT &
   elif [[ $PORT == "" ]] && [[ $TPORT == "" ]] && [[ $UPORT != "" ]] ; then
     echo "there is no way to set UDP only"
     exit
   fi
 
-  sed -i 's/socket-file = ocserv.sock/socket-file = ocserv$POSTFIX.sock/' /etc/ocserv/ocserv$POSTFIX.conf
-  sed -i 's/chroot-dir = \/var\/lib\/ocserv/chroot-dir = \/var\/lib\/ocserv$POSTFIX/' /etc/ocserv/ocserv$POSTFIX.conf
-  sed -i 's/pid-file = \/var\/run\/ocserv.pid/pid-file = \/var\/run\/ocserv$POSTFIX.pid/' /etc/ocserv/ocserv$POSTFIX.conf
-  sed -i 's/device = vpns/device = vpn$POSTFIXs/' /etc/ocserv/ocserv$POSTFIX.conf
-  sed -i 's/ipv4-network = 192.168.128.0/ipv4-network = 192.168.1${POSTFIX: -2}.0/' /etc/ocserv/ocserv$POSTFIX.conf
+  sed -i "s/socket-file = ocserv.sock/socket-file = ocserv$POSTFIX.sock/" /etc/ocserv/ocserv$POSTFIX.conf
+  sed -i "s/chroot-dir = \/var\/lib\/ocserv/chroot-dir = \/var\/lib\/ocserv$POSTFIX/" /etc/ocserv/ocserv$POSTFIX.conf
+  sed -i "s/pid-file = \/var\/run\/ocserv.pid/pid-file = \/var\/run\/ocserv$POSTFIX.pid/" /etc/ocserv/ocserv$POSTFIX.conf
+  sed -i "s/device = vpns/device = vpn$POSTFIXs/" /etc/ocserv/ocserv$POSTFIX.conf
+  sed -i "s/ipv4-network = 192.168.128.0/ipv4-network = 192.168.1${POSTFIX: -2}.0/" /etc/ocserv/ocserv$POSTFIX.conf
 
   if [[ $FAMILY ]] ; then
-    sed -i 's/dns = 1.1.1.1/dns = 1.1.1.3/' /etc/ocserv/ocserv$POSTFIX.conf
-    sed -i 's/dns = 8.8.8.8/dns = 1.0.0.3/' /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/dns = 1.1.1.1/dns = 1.1.1.3/" /etc/ocserv/ocserv$POSTFIX.conf
+    sed -i "s/dns = 8.8.8.8/dns = 1.0.0.3/" /etc/ocserv/ocserv$POSTFIX.conf
   fi
 
   cp -f /var/run/ocserv.pid /var/run/ocserv$POSTFIX.pid
@@ -112,9 +112,9 @@ if [[ ! $UPDATE ]] ; then
   cp -f /etc/systemd/system/ocserv.service /etc/systemd/system/ocserv$POSTFIX.service &
   wait
 
-  sed -i 's/PIDFile=\/var\/run\/ocserv.pid/PIDFile=\/var\/run\/ocserv$POSTFIX.pid/' /etc/systemd/system/ocserv$POSTFIX.service &
-  sed -i 's/ExecStartPre=\/usr\/sbin\/ocserv-genkey/ExecStartPre=\/usr\/sbin\/ocserv$POSTFIX-genkey/' /etc/systemd/system/ocserv$POSTFIX.service &
-  sed -i 's/ExecStart=\/usr\/sbin\/ocserv --pid-file \/var\/run\/ocserv.pid --config \/etc\/ocserv\/ocserv.conf -f/ExecStart=\/usr\/sbin\/ocserv$POSTFIX --pid-file \/var\/run\/ocserv$POSTFIX.pid --config \/etc\/ocserv\/ocserv$POSTFIX.conf -f/' /etc/systemd/system/ocserv$POSTFIX.service &
+  sed -i "s/PIDFile=\/var\/run\/ocserv.pid/PIDFile=\/var\/run\/ocserv$POSTFIX.pid/" /etc/systemd/system/ocserv$POSTFIX.service &
+  sed -i "s/ExecStartPre=\/usr\/sbin\/ocserv-genkey/ExecStartPre=\/usr\/sbin\/ocserv$POSTFIX-genkey/" /etc/systemd/system/ocserv$POSTFIX.service &
+  sed -i "s/ExecStart=\/usr\/sbin\/ocserv --pid-file \/var\/run\/ocserv.pid --config \/etc\/ocserv\/ocserv.conf -f/ExecStart=\/usr\/sbin\/ocserv$POSTFIX --pid-file \/var\/run\/ocserv$POSTFIX.pid --config \/etc\/ocserv\/ocserv$POSTFIX.conf -f/" /etc/systemd/system/ocserv$POSTFIX.service &
   wait
 
   systemctl daemon-reload &
