760b9880b1d3dc958c2992c8aa7a6a715127c5cc
xhdix <xhdix@yahoo.com>
2020-11-04 02:52:44 +0300

    Update familyuser.sh

--- familyuser.sh
+++ familyuser.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 # bash ./familyuser.sh username
 
-sed -i "s/#config-per-user = \/etc\/ocserv\/config-per-user\//config-per-user = \/etc\/ocserv\/config-per-user\//" /etc/ocserv/ocserv.conf &
-wait
+if grep -Fxq '#config-per-user = /etc/ocserv/config-per-user/' /etc/ocserv/ocserv.conf ; then
+
+  sed -i "s/#config-per-user = \/etc\/ocserv\/config-per-user\//config-per-user = \/etc\/ocserv\/config-per-user\//" /etc/ocserv/ocserv.conf &
+  wait
 
 
-mkdir -p /etc/ocserv/config-per-user/ &
-wait
-
-echo "
-dns = 1.0.0.3
-dns = 1.1.1.3
-" >  /etc/ocserv/config-per-user/$1 &
-wait
+  mkdir -p /etc/ocserv/config-per-user/ &
+  wait
 
 
-echo "done!"
-echo "now run: systemctl restart ocserv"
+  echo "
+  dns = 1.0.0.3
+  dns = 1.1.1.3
+  " >  /etc/ocserv/config-per-user/$1 &
+  wait
+
+
+  echo "done!"
+  echo "now run: systemctl restart ocserv"
+
+else
+
+
+  echo "
+  dns = 1.0.0.3
+  dns = 1.1.1.3
+  " >  /etc/ocserv/config-per-user/$1 &
+  wait
+  
+  echo "done!"
+
+fi
