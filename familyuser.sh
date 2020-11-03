ea59e8e02ae737c382c7bfd865f9f086899a127d
xhdix <xhdix@yahoo.com>
2020-11-04 01:38:21 +0300

    Create familyuser.sh

---
+++ familyuser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# bash ./familyuser.sh username
+
+sed -i "s/#config-per-user = \/etc\/ocserv\/config-per-user\//config-per-user = \/etc\/ocserv\/config-per-user\//" /etc/ocserv/ocserv.conf &
+wait
+
+
+mkdir -p /etc/ocserv/config-per-user/ &
+wait
+
+echo "
+dns = 1.0.0.3
+dns = 1.1.1.3
+" >  /etc/ocserv/config-per-user/$1 &
+wait
+
+
+echo "done!"
+echo "now run: systemctl restart ocserv"
